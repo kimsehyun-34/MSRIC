5c01fbdf8126e0f93a1138dab39c4c566e8796b6
Sehyun_Kim <79848348+FURY312@users.noreply.github.com>
2023-10-29 21:28:09 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.use('/public', express.static('public'));
+
+app.listen(8080, '0.0.0.0');
+
+console.log('실행: http://localhost:8080');
+
+//--
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/public/html/main.html')
+});
